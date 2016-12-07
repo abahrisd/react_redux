e7cf9650584cb99d0ff825c2810eabd5b6c546eb
SAbahri <SAbahri@ibs.ru>
2016-12-07 19:34:47 +0500

    use devTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,19 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import counter from '../reducer'
+import logger from '../middlewares/logger'
+import { applyMiddleware } from 'redux'
+import DevTools from '../containers/DevTools'
 
-const store = createStore(counter, {});
+const enhancer = compose(
+    applyMiddleware(logger),
+    DevTools.instrument()
+)
 
+const store = createStore(counter, {}, enhancer);
+
+if (module.hot) {
+    module.hot.accept('../reducer', () =>
+        store.replaceReducer(require('../reducer').default)
+    );
+}
 export default store
